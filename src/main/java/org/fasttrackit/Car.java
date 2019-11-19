b4c0969ec64b4199c3ee58e73e405b5b3150eaf3
laurentiu-meghesan <laurentiu.meghesan@gmail.com>
2019-11-19 22:39:54 +0200

    added new properties

---
+++ src/main/java/org/fasttrackit/Car.java
@@ -0,0 +1,12 @@
+package org.fasttrackit;
+
+public class Car extends Vehicle {
+
+    int doorCount;
+    String fuelType;
+    int motorSize;
+    boolean nitrogen;
+    boolean esp;
+
+
+}
---
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -0,0 +1,15 @@
+package org.fasttrackit;
+
+public class Vehicle {
+
+    String name;
+    String color;
+    double mileage;
+    double maxSpeed;
+    double fuelLevel;
+    double travelDistance;
+    boolean running;
+    int manufactureYear;
+    int weight;
+
+}
